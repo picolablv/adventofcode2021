21b45ce38521fbd058546ef4f0df4de47b00df55
andris <andris@kosmodroms.lv>
2021-12-09 17:28:23 +0200

    Adventofcode first challenge part2

--- 1.php
+++ 1.php
@@ -13,3 +13,29 @@ foreach ($input->rows() as $row) {
 }
 
 echo $increased;
+
+
+echo "\n============ part 2 ==============\n";
+
+$input = new FileReader('inputs/1.txt');
+
+$rows = [];
+
+$increased = 0;
+
+foreach ($input->rows() as $row) {
+    $rows[] = $row;
+}
+
+do {
+    $measurement1 = array_slice($rows, 0, 3);
+    array_shift($rows);
+    $measurement2 = array_slice($rows, 0, 3);
+
+    if (array_sum($measurement1) < array_sum($measurement2)) {
+        $increased++;
+    }
+
+} while (count($rows) >= 3);
+
+echo $increased . "\n";

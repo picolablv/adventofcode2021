3beaccac925e5bb3964b132b338a8ca54187ef7c
andris <andris@kosmodroms.lv>
2021-12-13 18:50:49 +0200

    7. day 2nd part challenge.

--- 7.php
+++ 7.php
@@ -16,17 +16,22 @@ $count = count($positions);
 $sum = array_sum($positions);
 
 $average = round($sum / $count, 0);
-$fuel = [];
+$fuel = $fuel_part2 = [];
 
-$fuel = array_fill(min($positions), max($positions) - min($positions) + 1, 0);
+$fuel = $fuel_part2 = array_fill(min($positions), max($positions) - min($positions) + 1, 0);
 
 for ($i = max($positions); $i >= min($positions); $i--) {
     foreach ($positions as $pos) {
         $steps = abs((int)$i - (int)$pos);
         $fuel[$i] += $steps;
 
+        for ($n = 1; $n <= $steps; $n++) {
+            $fuel_part2[$i] += $n;
+        }
     }
 }
 
 echo "most econ. FUEL:" . min($fuel);
+echo "\n";
+echo "most econ. FUEL part 2 : " . min($fuel_part2);
 echo "\n";
